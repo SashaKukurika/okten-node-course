1a52006fe24f594c9b3d92b8fdd5336f16004959
SashaKukurika <Kukurika.sasha@gmail.com>
2023-03-08 17:00:43 +0200

    done HW2

--- fs.services.js
+++ fs.services.js
@@ -2,6 +2,7 @@ const fs = require('node:fs/promises');
 const path = require('path');
 
 const usersPath = path.join(process.cwd(), 'users.json');
+
 const fsReader = async () => {
     const data = await fs.readFile(usersPath);
     return JSON.parse(data.toString());
