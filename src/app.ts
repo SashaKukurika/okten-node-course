1f746bdc507ca8fe3f280dee56fc85e04b08f1b5
SashaKukurika <Kukurika.sasha@gmail.com>
2023-03-09 17:02:19 +0200

    at progress HW3

--- src/app.ts
+++ src/app.ts
@@ -1,88 +1,81 @@
-import express from 'express'
+import express, { Request, Response } from "express";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 const users = [
-    {
-        name: 'Oleh',
-        age: 19,
-        gender: 'male'
-    },
-    {
-        name: 'Anton',
-        age: 22,
-        gender: 'female'
-    },
-    {
-        name: 'Anya',
-        age: 25,
-        gender: 'female'
-    },
-    {
-        name: 'Ielizavetta',
-        age: 35,
-        gender: 'female'
-    },
-    {
-        name: 'Cocos',
-        age: 70,
-        gender: 'mixed'
-    }
-]
+  {
+    name: "Oleh",
+    age: 19,
+    gender: "male",
+  },
+  {
+    name: "Anton",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "Anya",
+    age: 25,
+    gender: "female",
+  },
+  {
+    name: "Ielizavetta",
+    age: 35,
+    gender: "female",
+  },
+  {
+    name: "Cocos",
+    age: 70,
+    gender: "mixed",
+  },
+];
 
-app.get('/users', (req, res)=>{
-    res.json(users);
+app.get("/users", (req: Request, res: Response) => {
+  res.json(users);
 });
 
-//sdfsdf
-app.get('/users/:userId', (req, res)=>{
-    const { userId } = req.params;
-    const user = users[+userId];
+app.get("/users/:userId", (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const user = users[+userId];
 
-    res.json(user);
+  res.json(user);
 });
 
-app.post('/users', (req, res)=>{
-    const body = req.body;
-    users.push(body);
+app.post("/users", (req: Request, res: Response) => {
+  const body = req.body;
+  users.push(body);
 
-    res.status(201).json({
-        message: 'User created!'
-    })
-})
-
-
-app.put('/users/:userId', (req, res)=>{
-    const { userId } = req.params;
-    users[+userId] = req.body;
-
-    res.status(200).json({
-        message: 'User updated',
-        data: users[+userId]
-    })
-})
-
-app.delete('/users/:userId', (req, res)=>{
-    const { userId } = req.params;
-
-    users.splice(+userId, 1);
-
-    res.status(200).json({
-        message: 'User deleted',
-    })
-})
-
-
-
-
-
-app.get('/welcome', (req, res)=>{
-    res.send('WELCOME');
+  res.status(201).json({
+    message: "User created!",
+  });
 });
 
+app.put("/users/:userId", (req: Request, res: Response) => {
+  const { userId } = req.params;
+  users[+userId] = req.body;
+
+  res.status(200).json({
+    message: "User updated",
+    data: users[+userId],
+  });
+});
+
+app.delete("/users/:userId", (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  users.splice(+userId, 1);
+
+  res.status(200).json({
+    message: "User deleted",
+  });
+});
+
+app.get("/welcome", (req: Request, res: Response) => {
+  res.send("WELCOME");
+});
 
 // app.post()
 // app.put()
@@ -91,6 +84,6 @@ app.get('/welcome', (req, res)=>{
 
 const PORT = 5100;
 
-app.listen(PORT, ()=>{
-    console.log(`Server has started on PORT ${PORT} 🚀🚀🚀`);
+app.listen(PORT, () => {
+  console.log(`Server has started on PORT ${PORT} 🚀🚀🚀`);
 });
